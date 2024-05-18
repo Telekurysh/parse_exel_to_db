f9da16cb84ad752f0c1ed923acd33e4ddf2f6b38
Telekurysh <mic.mamaev@yandex.ru>
2024-05-18 17:41:25 +0300

    add foarm to upload

---
+++ app.py
@@ -0,0 +1,37 @@
+from flask import Flask, request, render_template, redirect, url_for
+import os
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = 'uploads'
+app.config['ALLOWED_EXTENSIONS'] = {'xls', 'xlsx'}
+
+if not os.path.exists(app.config['UPLOAD_FOLDER']):
+    os.makedirs(app.config['UPLOAD_FOLDER'])
+
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in app.config['ALLOWED_EXTENSIONS']
+
+
+@app.route('/')
+def upload_form():
+    return render_template('upload.html')
+
+
+@app.route('/upload', methods=['POST'])
+def upload_file():
+    if 'file' not in request.files:
+        return redirect(request.url)
+    file = request.files['file']
+    if file.filename == '':
+        return redirect(request.url)
+    if file and allowed_file(file.filename):
+        filename = file.filename
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        return 'Файл успешно загружен!'
+    else:
+        return 'Недопустимый тип файла. Пожалуйста, загрузите файл с расширением .xlsx или .xls'
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
